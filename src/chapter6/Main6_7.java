4d75184259ab5b60ad7b43df829d7e62b98ce594
bj-an <bgan0604@naver.com>
2023-02-06 20:51:41 +0900

    6-7. 좌표 정렬(compareTo)
    6-8. 이분검색
    
    
    6-9. 뮤직비디오(결정알고리즘)**retry
    
    
    6-10. 마구간 정하기(결정알고리즘)**retry

--- src/chapter6/Main6_7.java
+++ src/chapter6/Main6_7.java
@@ -2,12 +2,35 @@ package chapter6;
 
 import java.util.*;
 
+ class Point implements Comparable<Point>{
+	public  int x,y;
+	Point(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+	@Override
+	public int compareTo(Point o) {
+		// TODO Auto-generated method stub
+		if(this.x==o.x) return this.y-o.y;
+		else return this.x - o.x;
+	}
+	
+	
+}
 public class Main6_7 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
+		int n = sc.nextInt();
+		ArrayList<Point> arr = new ArrayList<Point>();
+		for(int i=0; i<n; i++) {
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			
+			arr.add(new Point(x,y));
+			
+		}
+		Collections.sort(arr);
+		for(Point o : arr) System.out.println(o.x+" "+o.y);
 		
 		
 		
